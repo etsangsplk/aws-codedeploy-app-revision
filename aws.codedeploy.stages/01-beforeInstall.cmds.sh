30a53143fa57801d88b7dc53b13e5e40f0a36104
Georgios Fisaris <gfisaris@gmail.com>
2016-10-11 16:52:01 +0100

    Add appspec.yml and other scripts

---
+++ aws.codedeploy.stages/01-beforeInstall.cmds.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+die() {
+    echo "FATAL ERROR: $* (status $?)" 1>&2
+    echo "Aborting Deployment.." 1>&2
+    exit 1
+}
+mkdir -p /aws.services/codedeploy/logs
+touch /aws.services/codedeploy/logs/latestDeployment.logs
+chmod 777 /aws.services/codedeploy/logs/latestDeployment.logs
+
+echo "START | Executing All Commands part of the "Before-Install" LifeCycle.." >> /aws.services/codedeploy/logs/latestDeployment.logs
+
+echo "Coping and Applying SSHd Configuration From S3.Bucket TO Root User" >> /aws.services/codedeploy/logs/latestDeployment.logs
+cd ~
+rm -rf ~/.ssh
+mkdir -p ~/.ssh
+aws s3 sync s3://global.general/SSH.Config.Files/.ssh/ ~/.ssh/ || die "Unable to Sync SSHd Config Files from S3.Bucket.."
+chmod 775 ~/.ssh
+chmod 600 ~/.ssh/*
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa || die "Unable to Update SSH Agent.."
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+#echo "Install GIT Software.." >> /aws.services/codedeploy/latestDeployment.logs
+#yum install -y git || die "Unable to install GIT.."
+
+echo "Cloning bridgemanart/AWS.CodeDeploy.Scripts.git Repo.." >> /aws.services/codedeploy/logs/latestDeployment.logs
+cd /aws.services/codedeploy
+rm -rf AWS.CodeDeploy.Scripts/
+git clone git@github.com:bridgemanart/AWS.CodeDeploy.Scripts.git || die "Unable to Clone bridgemanart/AWS.CodeDeploy.Scripts.git"
+chmod -R 777 AWS.CodeDeploy.Scripts/
+
+echo "FINISH | Executing All Commands part of the "Before-Install" LifeCycle.." >> /aws.services/codedeploy/logs/latestDeployment.logs
---
+++ aws.codedeploy.stages/03-applicationStart.cmds.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+die() {
+    echo "FATAL ERROR: $* (status $?)" 1>&2
+    echo "Aborting Deployment.." 1>&2
+    exit 1
+}
+
+echo "START | Executing all Commands that are part of "Application-Start" LifeCycle.." >> /aws.services/codedeploy/logs/latestDeployment.logs
+cd /aws.services/codedeploy/AWS.CodeDeploy.Scripts
+sh execute.application.start.cmds.sh || die "Execution of a Command Failed! Please check Deployment logs for more informations.."
+echo "FINISH | Executing all Commands that are part of "Application-Start" LifeCycle.." >> /aws.services/codedeploy/logs/latestDeployment.logs

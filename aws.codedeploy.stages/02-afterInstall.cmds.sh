3f1995b4d62ccbd6bb4eda44de3e84fcea364bb8
Georgios Fisaris <gfisaris@gmail.com>
2016-10-16 06:42:19 +0100

    Update 02-afterInstall.cmds.sh

--- aws.codedeploy.stages/02-afterInstall.cmds.sh
+++ aws.codedeploy.stages/02-afterInstall.cmds.sh
@@ -7,5 +7,5 @@ die() {
 }
 
 echo "## START "After-Install" LifeCycle.."
-bash /aws.services/codedeploy/AWS.CodeDeploy.Scripts/execute.after-install.cmds.sh || die "Execution of a Command Failed! Please check Deployment logs for more informations.."
+    bash /aws.services/codedeploy/aws-ec2-instance-provision-scripts/execute.after-install.cmds.sh || die "Execution of a Command Failed! Please check Deployment logs for more informations.."
 echo "## FINISH "After-Install" LifeCycle.."

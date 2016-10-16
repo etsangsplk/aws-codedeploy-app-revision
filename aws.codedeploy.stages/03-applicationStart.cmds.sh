abc397a36755b266627a81e3c354ba1466456461
Georgios Fisaris <gfisaris@gmail.com>
2016-10-16 06:45:52 +0100

    Update 03-applicationStart.cmds.sh

--- aws.codedeploy.stages/03-applicationStart.cmds.sh
+++ aws.codedeploy.stages/03-applicationStart.cmds.sh
@@ -6,6 +6,6 @@ die() {
     exit 1
 }
 
-echo "START "Application-Start" LifeCycle.."
+echo "START CodeDeploy LifeCycle: \"Application Start\""
     bash /aws.services/codedeploy/aws-ec2-instance-provision-scripts/execute.application-start.cmds.sh || die "Execution of a Command Failed! Please check Deployment logs for more informations.."
-echo "FINISH "Application-Start" LifeCycle.."
+echo "END CodeDeploy LifeCycle: \"Application Start\""

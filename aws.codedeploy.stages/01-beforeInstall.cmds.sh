f555df49c94c8b62b20c3c55e6949ecb0f1f2441
Georgios Fisaris <gfisaris@gmail.com>
2016-10-16 06:37:47 +0100

    Update 01-beforeInstall.cmds.sh

--- aws.codedeploy.stages/01-beforeInstall.cmds.sh
+++ aws.codedeploy.stages/01-beforeInstall.cmds.sh
@@ -6,39 +6,51 @@ die() {
     exit 1
 }
 
-while [ -f /tmp/cloud-init.running ];
-do
-    echo "## Cloud-Initialization still running.."
-    echo "## Check again in 5 seconds.."
-    sleep 5
-done
-echo "## Cloud-Initialization finished.."
+echo "## Check IF cloud-init lock exist"
+    while [ -f /tmp/cloud-init.running ];
+    do
+        echo "##-- Cloud-Initialization still running"
+        echo "##-- Check again in 5 seconds"
+        sleep 5
+    done
+echo "## Cloud-Initialization finished"
+
+echo "## Clear any pre-existing custom CodeDeploy-Deployment Logs Folder"
+    if [ -d "/aws.services/codedeploy/logs" ];
+        then
+            rm -rf /aws.services/codedeploy/logs
+    fi
+
+echo "## Create Folder: CodeDeploy-Deployment Logs"
+    mkdir -p /aws.services/codedeploy/logs
+
+echo "## Create File: latestDeployment.logs"
+    touch /aws.services/codedeploy/logs/latestDeployment.logs
+    chmod 777 /aws.services/codedeploy/logs/latestDeployment.logs
+
+## CodeDeploy LifeCycle: "Before Install"
+
+echo "## START "Before-Install" LifeCycle"
+
+echo "## Coping and Applying SSHd Configuration From S3.Bucket FOR Root User"
+    rm -rf ~/.ssh
+    mkdir -p ~/.ssh
+    aws s3 cp s3://global.general/SSH.Config.Files/.ssh/ ~/.ssh/ || die "Unable to Copy SSHd Config Files from S3.Bucket"
+    chmod 775 ~/.ssh
+    chmod 600 ~/.ssh/*
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_rsa || die "Unable to Update SSH Agent"
+    echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 
-mkdir -p /aws.services/codedeploy/logs
-touch /aws.services/codedeploy/logs/latestDeployment.logs
-chmod 777 /aws.services/codedeploy/logs/latestDeployment.logs
+echo "## Delete, if any, pre-existing Folder: aws-ec2-instance-provision-scripts"
+    if [ -d "/aws.services/codedeploy/aws-ec2-instance-provision-scripts" ];
+        then
+            rm -rf /aws.services/codedeploy/laws-ec2-instance-provision-scripts
+    fi
 
-echo "START | Executing All Commands part of the "Before-Install" LifeCycle.." >> /aws.services/codedeploy/logs/latestDeployment.logs
+echo "## Cloning Git Repo: aws-ec2-instance-provision-scripts"
+    git clone git@github.com:gfisaris/aws-ec2-instance-provision-scripts.git /aws.services/codedeploy/ || die "Unable to Clone GIT Repository"
+    chmod -R 777 aws-ec2-instance-provision-scripts/
 
-echo "Coping and Applying SSHd Configuration From S3.Bucket TO Root User" >> /aws.services/codedeploy/logs/latestDeployment.logs
-cd ~
-rm -rf ~/.ssh
-mkdir -p ~/.ssh
-aws s3 sync s3://global.general/SSH.Config.Files/.ssh/ ~/.ssh/ || die "Unable to Sync SSHd Config Files from S3.Bucket.."
-chmod 775 ~/.ssh
-chmod 600 ~/.ssh/*
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa || die "Unable to Update SSH Agent.."
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-
-#echo "Install GIT Software.." >> /aws.services/codedeploy/latestDeployment.logs
-#yum install -y git || die "Unable to install GIT.."
-
-echo "Cloning bridgemanart/AWS.CodeDeploy.Scripts.git Repo.." >> /aws.services/codedeploy/logs/latestDeployment.logs
-cd /aws.services/codedeploy
-rm -rf AWS.CodeDeploy.Scripts/
-git clone git@github.com:gfisaris/aws-ec2-instance-provision-scripts.git || die "Unable to Clone bridgemanart/AWS.CodeDeploy.Scripts.git"
-chmod -R 777 AWS.CodeDeploy.Scripts/
-
-echo "FINISH | Executing All Commands part of the "Before-Install" LifeCycle.." >> /aws.services/codedeploy/logs/latestDeployment.logs
+echo "## FINISH "Before-Install" LifeCycle.."
